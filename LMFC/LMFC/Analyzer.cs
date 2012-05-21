e574c7bd4d221fcd94abfc4801c4d3557c0381af
Anton Chaika <jonyinweb@gmail.com>
2012-05-22 00:28:29 +0300

    The newest version of LMFC lib

---
+++ LMFC/LMFC/Analyzer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace ThematicAnalyser
+{
+    public class IE_Value
+    {
+        public Element IE;
+        public double Value;
+        public IE_Value(Element ie, double val)
+        {
+            IE = ie;
+            Value = val;
+        }
+    }
+    class Analyzer
+    {
+        private SortedList allIE = new SortedList();
+        /// <summary>
+        /// Извлекает ключевые слова из текста с учетом контекста и количества повторений.
+        /// </summary>
+        /// <param name="threshold">Порог в процентах, необходимый для определения первичного множества ключевых элементов.</param>
+        /// <param name="range">Окрестность, в пределах которой осуществляется контекстный анализ.</param>
+        /// <param name="screen">Параметр отсечки, используется для исключения из множества ключевых элементов тех элементов, количество повторений которых меньше или равно screen.</param>
+        /// <returns>SortedList</returns>
+        public SortedList GetKey(int threshold, int range, int screen)
+        {
+            ArrayList key = GetPrimaryKey(threshold);
+            key = GetExpandPrimary(key, range, screen);
+            SortedList key_sl = ConvertToSortedList(key);
+            Reduction(key_sl);
+            return key_sl;
+        }
+
+        private ArrayList GetPrimaryKey(int threshold)
+        {
+            int max = GetMaxInOut();
+            int val = (int)((max / 100.0) * threshold);
+            ArrayList result = new ArrayList();
+            foreach (Element ie in allIE.Values)
+            {
+                if (ie.InOut.Count >= val)
+                {
+                    result.Add(new IE_Count(ie, ie.InOut.Count));
+                }
+            }
+            return result;
+        }
+        private int GetMaxInOut()
+        {
+            int max = 0;
+            foreach (Element ie in allIE.Values)
+            {
+                if (max < ie.InOut.Count) max = ie.InOut.Count;
+            }
+            return max;
+        }
+
+        private ArrayList GetExpandPrimary(ArrayList primary, int range, int screen)
+        {
+            Searcher search = new Searcher();
+            ArrayList result = search.SearchContext(ConvertToSortedList(primary), range, screen);
+            return result;
+        }
+
+        static private SortedList ConvertToSortedList(ArrayList al)
+        {
+            if (al == null) return null;
+            SortedList result = new SortedList();
+            foreach (IE_Count ie_count in al)
+            {
+                result.Add(ie_count.IE.Name, new IE_Value(ie_count.IE, ie_count.Count));
+            }
+            return result;
+        }
+
+        static private void Reduction(SortedList sl)
+        {
+            // общая сумма
+            double sum = 0;
+            foreach (IE_Value ie_value in sl.Values) sum += ie_value.Value;
+            foreach (IE_Value ie_value in sl.Values)
+            {
+                double k = ie_value.Value / sum;
+                ie_value.Value = k;
+            }
+        }
+    }
+}
---
+++ LMFC/LMFC/Morpho.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using LEMMATIZERLib;
+using AGRAMTABLib;
+
+namespace ThematicAnalyser
+{
+    class Morpho
+    {
+        ArrayList al;
+        ILemmatizer lemmatizerRus;
+        /// <summary>
+        /// Get norm form of the word.
+        /// </summary>
+        /// <param name="words">Words after lexical analyse</param>
+        /// <param name="ignor">List of stop-words</param>
+        /// <returns></returns>
+        public ArrayList GetLemmas(IEnumerable<string> words, string[] ignor)
+        {
+            lemmatizerRus.LoadDictionariesRegistry();
+            foreach (var word in words)
+            {
+                ParadigmCollection parCol = null;
+                //finding norm of the word
+                //if (word.IndexOfAny("abcdefghijklmnopqrstuvwxyz".ToCharArray()) == -1)
+                //{
+                    parCol = lemmatizerRus.CreateParadigmCollectionFromForm(word, 0, 1);
+                //}
+                //else
+                //{
+                //    parCol = lemmatizerEng.CreateParadigmCollectionFromForm(word, 0, 1);
+                //}
+                if (parCol.Count > 0)
+                {
+                    al.Add(parCol[0].Norm.ToLower());
+                }
+            }
+            int[] num = new int[al.Count];
+            int i = 0, j = 0;
+            foreach (string word in al)
+            {
+                if (ignor.Contains(word))
+                {
+                    num[j++] = i;
+                }
+                i++;
+            }
+            if ((num[0] == 0 && num[1] != 0) || (num[0] != 0))
+            {
+                for (i = 0; i < num.Count(); i++)
+                {
+                    al.RemoveAt(num[i] - i);
+                    if (num[i + 1] == 0)
+                        break;
+                }
+            }
+            return al;
+        }
+        public Morpho()
+        {
+            lemmatizerRus = new LemmatizerRussian();
+            al = new ArrayList();
+        }
+    }
+}
